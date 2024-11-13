c15c34158ba1794005a3365a68d48b43cc7e7e6e
PaulinaGalicia <paulina.galicia@citi.com.mx>
2024-11-13 15:37:13 -0600

    new folder

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- project/app/src/main/java/com/citi/scanner/MainActivity.kt
+++ project/app/src/main/java/com/citi/scanner/MainActivity.kt
@@ -1,11 +1,2 @@
 package com.citi.scanner
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-}
