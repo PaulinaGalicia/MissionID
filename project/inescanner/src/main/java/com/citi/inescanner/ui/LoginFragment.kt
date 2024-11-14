f542ff2c547e3ded62d134ae17f1a6388b90a32c
Christian Alexis <christian.perez@citi.com.mx>
2024-11-14 11:42:17 -0600

    Modificacion de loginFragment

--- project/inescanner/src/main/java/com/citi/inescanner/ui/LoginFragment.kt
+++ project/inescanner/src/main/java/com/citi/inescanner/ui/LoginFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.citi.inescanner.R
 import com.citi.inescanner.databinding.FragmentLoginBinding
+import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment() {
 
@@ -34,8 +36,15 @@ class LoginFragment : Fragment() {
             viewModel.login(username, password)
         }
 
-        //inicio de sesión
-
+        viewModel.loginState.observe(viewLifecycleOwner){ isSuccess ->
+            if(isSuccess){
+                // credenciales correctas
+                findNavController().navigate(R.id.action_loginFragment_to_scannerFragment)
+            }else{
+                // credenciales incorrectas
+                Toast.makeText(context, "Credenciales incorrectas", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
--- project/inescanner/src/main/java/com/citi/inescanner/ui/LoginViewModel.kt
+++ project/inescanner/src/main/java/com/citi/inescanner/ui/LoginViewModel.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
 
+    val _loginState = MutableLiveData<Boolean>()
+    val loginState: LiveData<Boolean> get() = _loginState
+
     private val hardcodedUsername = "Admin"
     private val hardcodedPassword = "qwerty"
 
     fun login(username: String, password: String) {
-
+        _loginState.value = (username == hardcodedUsername && password == hardcodedPassword)
     }
 }
